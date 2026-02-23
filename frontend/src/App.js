ceb316d8f5b73a0014175087bb74bbbc25562c91
aryan <aryanthawakar975@gmail.com>
2026-02-23 12:32:55 +0530

    init

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("Loading...");
+
+  useEffect(() => {
+    const API_URL =
+      process.env.NODE_ENV === "production"
+        ? "/api/health"
+        : "http://localhost:5000/api/health";
+
+    fetch(API_URL)
+      .then(res => res.json())
+      .then(data => setMessage("CI/CD Working 🚀 + 123"))
+      .catch(() => setMessage("API Error"));
+  }, []);
+
+  return (
+    <div>
+      <h1>{message}</h1>
+    </div>
+  );
+}
+
+export default App;
